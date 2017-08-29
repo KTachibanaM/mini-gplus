97fd1a95c8d4548a80a5cc1139fb8eb9ce95aba9
KT233 <whj19931115@gmail.com>
2017-08-29 19:47:26 +0000

    lol

--- post_endpoint.py
+++ post_endpoint.py
@@ -1,27 +1,25 @@
 from urlparse import urlparse, urljoin
-from flask import request
+from flask import redirect
 from flask_login import login_required
 
 
-def is_safe_url(target):
+def is_safe_url(request, target):
     ref_url = urlparse(request.host_url)
     test_url = urlparse(urljoin(request.host_url, target))
     return test_url.scheme in ('http', 'https') and ref_url.netloc == test_url.netloc
 
 
-def get_redirect_target():
+def get_redirect_target(request):
     for target in request.values.get('next'), request.referrer:
         if not target:
             continue
         if is_safe_url(target):
             return target
+    return None
 
-
-def post_endpoint(app, route):
-    @app.route(route, methods=['POST'])
-    @login_required
-    def decorator(route_func):
-        def wrapper(*args, **kwargs):
-            route_func(*args, **kwargs)
-        return wrapper
-    return decorator
+def redirect_back(request, default_target='/'):
+    redirect_target = get_redirect_target(request)
+    if not redirect_target:
+        return redirect(default_target)
+    else:
+        return redirect(redirect_target)
